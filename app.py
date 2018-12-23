d14712b14dfaa38543ccda55daf9f2f5bd8d7ce7
creativesites <46081328+creativesites@users.noreply.github.com>
2018-12-23 09:04:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def processRequest(req):
     searchString = "robot %s" % searchstring
 
     # KEYS SHOULDNT BE DISPLAYED
-    my_api_key = ""
-    my_cse_id = ""
+    my_api_key = "AIzaSyANVOhf9rUGGH6tWxUyC1H5fuPkNqo5oTA"
+    my_cse_id = "001478286758381537938:pn56uiityg4"
     searchResults = google_search(searchString, my_api_key, my_cse_id, num=1)    # search for the topic
 
     if searchResults is None:
